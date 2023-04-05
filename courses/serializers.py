be0d09a7d4013f7eef356910b68663ce5cda20c9
prasanaseyon <prasana.sean@gmail.com>
2023-04-05 10:41:28 +0530

    added comment in serializer.py

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,7 +1,9 @@
+#this is serializer
 from rest_framework import serializers
 from .models import Course
 
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Course
-        fields=('id','url','name','language','price')
+        fields=('id','url',
+                'name','language','price')
